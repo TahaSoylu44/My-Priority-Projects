98c8e65369ff196f356cd91e07c9e484e6aeb50e
TahaSoylu44 <thsoylu44@gmail.com>
2025-03-05 01:30:47 +0300

    I made a predicting a number game :)

---
+++ PredictMyNumberGame/src/Main.java
@@ -0,0 +1,37 @@
+import java.util.Random;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        //Today,I am going to make a game -> Predict my number :)
+
+        Random target = new Random();
+        Scanner input = new Scanner(System.in);
+
+        int number = target.nextInt(10);
+
+        System.out.print("Enter the number between 1 and 10: ");
+        int user = input.nextInt();
+
+        int counter = 0;
+
+        while(number != user){
+            if(user > number){
+                System.out.println("It is greater than our target,try again 😇");
+                System.out.print("Please enter the number: ");
+                user = input.nextInt();
+            }
+            else{
+                System.out.println("It is lower than our target,try again 😉");
+                System.out.print("Please enter the number: ");
+                user = input.nextInt();
+            }
+
+            counter += 1;
+        }
+
+        System.out.println("You hit the target.Target was " + number);
+        System.out.println("You tried it " + counter + " times.");
+
+        input.close();
+    }
+}
